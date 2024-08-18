9471dfd2f73d59ef5e99ff9f297cb8e34df776a2
MOHAMMED-IQRAMUL <MOHDIQRAMUL2468@GMAIL.COM>
2024-08-18 15:16:01 +0530

    update

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -16,7 +16,7 @@ const GoogleAnalytics = () => {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${GA_ID}');
+          gtag('config', ${GA_ID}); 
         `}
       </Script>
     </>
